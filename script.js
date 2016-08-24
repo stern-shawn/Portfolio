d8e5fec10496781dd2cd95e26cab7e212055cfe9
Shawn Stern <stern.shawn@gmail.com>
2016-08-23 23:27:15 -0700

    Adding link to Blog website! Includes new font awesome icon for blog on mobile view of site. Had to modify the JS to get this to work properly, as the e.preventDefault was preventing the navbar links from going to external sites. New conditional added to check for 'external' class on links so I can have both functionalities!

--- script.js
+++ script.js
@@ -2,18 +2,23 @@
 $(document).ready(function() {
   // Scroll only when user clicks a link contained in the navbar
   $(".navbar a").on('click', function(event) {
-    event.preventDefault();
+    // Adding a conditional statement so that navbar can contain external links
+    // while maintaining scroll animation for all other link elements
+    if (!$(this).hasClass("external")) {
+      event.preventDefault();
 
-    // Grab the hash element of the incoming href,
-    // ie returns #heading from <a href="#heading"...
-    var hash = this.hash;
+      // Grab the hash element of the incoming href,
+      // ie returns #heading from <a href="#heading"...
+      var hash = this.hash;
+      // console.log(hash)
 
-    // Animate using the 'scrollTop' property. IE. set scrollTop to the
-    // offset of the element in 'hash', take 700ms, use 'swing, and have a
-    // callback function for logging/debug...
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top }, 700, 'swing', function() {
-        //console.log("Scrolled to: " + $(hash).offset().top);
-    });
+      // Animate using the 'scrollTop' property. IE. set scrollTop to the
+      // offset of the element in 'hash', take 700ms, use 'swing, and have a
+      // callback function for logging/debug...
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top }, 700, 'swing', function() {
+          //console.log("Scrolled to: " + $(hash).offset().top);
+      });
+    }
   });
 });
